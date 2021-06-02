2abf453416580402f76da6b48979dedf617517ea
ValenAquino <vaquino@frba.utn.edu.ar>
2021-06-02 19:52:07 -0300

    Punto 3 completo

--- src/Library.hs
+++ src/Library.hs
@@ -1,8 +1,103 @@
 module Library where
 import PdePreludat
-import Text.Show.Functions
 import Data.Char (toUpper, isUpper)
 
 -- point free: cuando nos ahorramos de pedirle uno o mas parametros porque son a quienes aplicarle directamente una función establecida
 -- esMultiploDe a b = ((==0).( `mod` a)) b  ===>  la b se podría evitar, sin modificar la función   ===>   esMultiploDe a = (==0).( `mod` a)
+
+data Barbarian = Barbarian{
+    name :: String,
+    strength :: Number,
+    skills :: [Skill],
+    objects :: [Object]
+} deriving Show
+
+instance Eq Barbarian where
+  (Barbarian name strength skills objects) == (Barbarian name' strength' skills' objects') =
+     name == name'  && strength == strength' && skills == skills && length objects == length objects'
+
+type Object = Barbarian -> Barbarian
+type Skill = String
+type Aventura = Barbarian -> Bool
+type Evento = Barbarian -> Bool
+
+-- Punto 1
+
+espada :: Number -> Object
+espada swordLarge barbarian = barbarian {strength = strength barbarian + 2 * swordLarge}
+
+amuletoMisticoPuercoMarrano :: Skill -> Object
+amuletoMisticoPuercoMarrano skill barbarian = barbarian {skills = skills barbarian ++ [skill]}
+
+varitasDefectuosas :: Object
+varitasDefectuosas barbarian = barbarian {skills = skills barbarian ++ ["Hacer Magia"], objects = []}
+
+ardilla :: Object
+ardilla = id
+
+cuerda :: Object -> Object -> Object
+cuerda object1 object2 = object1 . object2
+
+
+-- Punto 2
+
+megafono :: Object
+megafono barbarian = barbarian {skills = map ponerEnMayuscula (skills barbarian) }
+
+ponerEnMayuscula :: String -> String
+ponerEnMayuscula = map toUpper
+
+megafonoMegaBarbarico :: Object
+megafonoMegaBarbarico = cuerda ardilla megafono
+
+
+-- Punto 3
+
+condicionDeSupervivencia :: [Evento] -> Barbarian -> Bool
+condicionDeSupervivencia condiciones barbaro = any ((== True) . (\condicion -> condicion barbaro)) condiciones
+
+
+invasionDeSuciosDuendes :: Aventura
+invasionDeSuciosDuendes = condicionDeSupervivencia [escribePoesiaAtroz]
+
+escribePoesiaAtroz :: Evento
+escribePoesiaAtroz barbaros =  "Escribir Poesía Atroz" `elem` skills barbaros
+
+
+cremalleraDelTiempo :: Aventura
+cremalleraDelTiempo = condicionDeSupervivencia [tenerPulgares]
+
+tenerPulgares :: Evento
+tenerPulgares barbarian = name barbarian == "Faffy" || name barbarian == "Astro"
+
+
+ritualDeFechorias :: Aventura
+ritualDeFechorias = condicionDeSupervivencia [saqueo, gritoDeGuerra, caligrafia]
+
+saqueo :: Evento
+saqueo barbaro = "Robar" `elem` skills barbaro || strength barbaro > 80
+
+gritoDeGuerra :: Evento
+gritoDeGuerra barbaro =  poderDeGritoDeGuerra barbaro == cantidadDeLetrasDeTodasSusHabilidades barbaro
+
+poderDeGritoDeGuerra :: Barbarian -> Number
+poderDeGritoDeGuerra = (4 *).length.objects
+
+cantidadDeLetrasDeTodasSusHabilidades :: Barbarian -> Number
+cantidadDeLetrasDeTodasSusHabilidades = sum . map length . skills
+
+caligrafia :: Evento
+caligrafia barbaro =  all tieneTresVocalesYempiezaConMayuscula (skills barbaro)
+
+tieneTresVocalesYempiezaConMayuscula :: String ->  Bool
+tieneTresVocalesYempiezaConMayuscula habilidad = tieneTresVocales habilidad && empiezaConMayuscula habilidad
+
+tieneTresVocales :: String -> Bool
+tieneTresVocales =  (== 3) . length . filter (`elem` lasVocales)
+
+empiezaConMayuscula :: String ->  Bool
+empiezaConMayuscula = isUpper.head
+
+lasVocales :: [Char]
+lasVocales = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
 
--- src/Spec.hs
+++ src/Spec.hs
@@ -4,23 +4,68 @@ import Library
 import Test.Hspec
 import Control.Exception (evaluate, AsyncException (ThreadKilled))
 
+-- Personas de ejemplo
+
+dave :: Barbarian 
+dave = Barbarian "Dave" 100 ["tejer","escribirPoesia"] [ardilla, espada 5]
+
+
+
 correrTests :: IO ()
 correrTests = hspec $ do
  
  describe "Punto 1" $ do
-   describe "" $ do
+   describe "espada" $ do
+      it "una espada que pesa 5 kilos le da a Dave 10 de fuerza extra" $ do
+         espada 5 dave `shouldBe` dave {strength = 110}
+   describe "amuletos" $ do
+      it "dave puede ser un ingeniero magico ahora que tiene su amuleto mistico puerco marrano" $ do
+         amuletoMisticoPuercoMarrano "Ingeniero Magico" dave `shouldBe` dave {skills = ["Ingeniero Magico","tejer","escribirPoesia"]}
+   describe "varitas defectuosas" $ do
+      it "Gracias a su varita defectuosa, dave ahora tiene la habilidad de Hacer magia" $ do
+         varitasDefectuosas dave `shouldBe`  dave { skills = ["tejer", "escribirPoesia", "Hacer Magia"], objects = []}
+   describe "ardillas" $ do
+      it "de objeto que nada se esperaba ... NADA ESTÁ HACIENDO" $ do
+         ardilla dave `shouldBe`  dave
+   describe "cuerda" $ do
+      it "Gracias a la cuerda, Dave ahora puede ser un ingeniero magico y hacer magia a la vez" $ do
+         cuerda varitasDefectuosas (amuletoMisticoPuercoMarrano "Ingeniero Magico") dave `shouldBe` dave { skills = ["tejer","escribirPoesia", "Ingeniero Magico", "Hacer Magia"], objects = []}
+
+ describe "Punto 2" $ do
+    describe "megafono" $ do
+      it "dave con su megafono POTENCIA TODAS SUS HABILIDADES" $ do
+         megafono dave `shouldBe` dave{ skills = ["TEJER","ESCRIBIRPOESIA"]}
+    describe "megafonoMegaBarbarico" $ do
+      it "dave con un megafono megabarbarico POTENCIA TODAS SUS HABILIDADES pero no mas que eso" $ do
+         megafonoMegaBarbarico dave `shouldBe` dave{ skills = ["TEJER","ESCRIBIRPOESIA"]}
+
+ describe "Punto 3" $ do
+    describe "Invasión de Duendes" $ do
+      it "Dave sobrevive a la invasion de duendes porque sabe escribir poesia atroz" $ do
+         invasionDeSuciosDuendes dave{skills = ["Escribir Poesía Atroz"]} `shouldBe` True 
+      it "Dave no sobrevive a la invasion de duendes porque no sabe escribir poesia atroz" $ do
+         invasionDeSuciosDuendes dave `shouldBe` False 
+    describe "Cremallera Del Tiempo" $ do
+      it "Dave no sobrevive porque tiene pulgares" $ do
+         cremalleraDelTiempo dave `shouldBe`  False 
+      it "Faffy y Astro sobreviven porque no tienen pulgares" $ do
+         cremalleraDelTiempo dave {name = "Faffy"} `shouldBe` True 
+         cremalleraDelTiempo dave {name = "Astro"} `shouldBe` True
+    describe "Ritual de Fechorias" $ do
+      it "Dave el barbaro ladrón sobrevive al ritual desde que empezó a pulir su habilidad de robar" $ do
+         ritualDeFechorias dave{skills = ["Robar"]} `shouldBe` True
+      it "Dave el barbaro gritón sobrevive al ritual" $ do
+         ritualDeFechorias dave{skills = ["tejer", "ver"]} `shouldBe` True
+      it "Dave el barbaro correcto sobrevive al ritual desde que mejoró su ortografía" $ do
+         ritualDeFechorias dave{skills = ["Cocinar", "Estudiar"]} `shouldBe` True
+
+    describe "" $ do
       it "" $ do
          2+2 `shouldBe`  4
-   
- describe "Punto 2" $ do
     describe "" $ do
       it "" $ do
          2+2 `shouldBe`  4
 
- describe "Punto 3" $ do
-    describe "" $ do
-      it "" $ do
-         2+2 `shouldBe`  4
 
  describe "Punto 4" $ do
     describe "" $ do
