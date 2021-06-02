7691fd472e0a11d8f4b372d0ac7cbf773336b119
ValenAquino <vaquino@frba.utn.edu.ar>
2021-06-02 10:48:38 -0300

    update

--- src/Library.hs
+++ src/Library.hs
@@ -2,3 +2,7 @@ module Library where
 import PdePreludat
 import Text.Show.Functions
 import Data.Char (toUpper, isUpper)
+
+-- point free: cuando nos ahorramos de pedirle uno o mas parametros porque son a quienes aplicarle directamente una función establecida
+-- esMultiploDe a b = ((==0).( `mod` a)) b  ===>  la b se podría evitar, sin modificar la función   ===>   esMultiploDe a = (==0).( `mod` a)
+
